30fa3b6016a9de0a2538e7247e57b74fd403725f
Techwolf <dring@g33kworld.net>
2017-03-02 15:38:32 -0800

    Fixed mesh startup script.

--- stage2/03-mesh/files/up.sh
+++ stage2/03-mesh/files/up.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Blink power LED
-echo gpio | sudo tee /sys/class/leds/led0/trigger
-echo 1 | sudo tee /sys/class/leds/led0/brightness
+echo gpio > /sys/class/leds/led0/trigger
+echo 1 > /sys/class/leds/led0/brightness
 sleep 1;
-echo 0 | sudo tee /sys/class/leds/led0/brightness
+echo 0 > /sys/class/leds/led0/brightness
 sleep 1;
-echo 1 | sudo tee /sys/class/leds/led0/brightness
+echo 1 > /sys/class/leds/led0/brightness
 sleep 1;
 
 
@@ -14,15 +14,15 @@ sleep 1;
 #https://github.com/o11s/open80211s/wiki/HOWTO
 MESH_ID="MESHNET"
 
-ip link set down dev wlan0
-iw dev wlan0 interface add mesh0 type mp
-#ip link set dev mesh0 address $NEW_MAC_ADDR
-ip link set up dev mesh0
-iw dev mesh0 mesh join $MESH_ID
-iw dev mesh0 station dump > /home/pi/mesh_stations
+/sbin/ip link set down dev wlan0
+/sbin/iw dev wlan0 interface add mesh0 type mp
+#/sbin/ip link set dev mesh0 address $NEW_MAC_ADDR
+/sbin/ip link set up dev mesh0
+/sbin/iw dev mesh0 mesh join $MESH_ID
+/sbin/iw dev mesh0 station dump > /home/pi/mesh_stations
 
 #TODO: Get/set IP addresses? IPv6 NDP?
 
 
 #Set power LED on
-echo 0 | sudo tee /sys/class/leds/led0/brightness
+echo 0 > /sys/class/leds/led0/brightness
