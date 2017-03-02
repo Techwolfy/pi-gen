3e62581ee8565316d2bd5eeef71130369a03c91d
Techwolf <dring@g33kworld.net>
2017-03-02 10:33:10 -0800

    Updated kernel modules to v4.4.48+.

--- stage2/03-mesh/01-run.sh
+++ stage2/03-mesh/01-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-#Custom RTL8192CU drivers
-RTLWIFI_DIR=lib/modules/4.4.38+/kernel/drivers/net/wireless/realtek/rtlwifi
+#Install custom RTL8192CU drivers
+RTLWIFI_DIR=lib/modules/4.4.48+/kernel/drivers/net/wireless/realtek/rtlwifi
 install -v -d											${ROOTFS_DIR}/${RTLWIFI_DIR}
 install -v -m 644 files/rtlwifi/rtlwifi.ko				${ROOTFS_DIR}/${RTLWIFI_DIR}/rtlwifi.ko
 install -v -m 644 files/rtlwifi/rtl_usb.ko				${ROOTFS_DIR}/${RTLWIFI_DIR}/rtl_usb.ko
@@ -9,15 +9,20 @@ install -v -m 644 files/rtlwifi/rtl8192c-common.ko		${ROOTFS_DIR}/${RTLWIFI_DIR}
 install -v -m 644 files/rtlwifi/rtl8192cu.ko			${ROOTFS_DIR}/${RTLWIFI_DIR}/rtl8192cu.ko
 install -v -m 644 files/blacklist-rtl8192cu.conf		${ROOTFS_DIR}/etc/modprobe.d/blacklist-rtl8192cu.conf
 
-#Startup script
+#Update module dependencies
+on_chroot << 'EOF'
+depmod -a $(ls -1 /lib/modules | head -n 1)
+EOF
+
+#Install startup script
 install -v -d											${ROOTFS_DIR}/etc/mesh
 install -v -m 755 files/up.sh							${ROOTFS_DIR}/etc/mesh/
 install -v -m 644 files/mesh							${ROOTFS_DIR}/etc/cron.d/
 
-#Web server config
+#Install web server config
 install -v -m 644 files/nginx.conf						${ROOTFS_DIR}/etc/nginx/nginx.conf
 
-#Website files
+#Install website files
 install -v -d											${ROOTFS_DIR}/var/www/mesh
 install -v -m 644 files/index.php						${ROOTFS_DIR}/var/www/mesh/index.php
 
