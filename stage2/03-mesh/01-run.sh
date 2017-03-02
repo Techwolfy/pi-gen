56351d5162428c5fbbda0b2c3876be4e8f8ffc77
Techwolf <dring@g33kworld.net>
2017-03-01 17:59:55 -0800

    Removed unused packages. Added custom RTL8192CU drivers. Updated nginx config and added placeholder web page.

--- stage2/03-mesh/01-run.sh
+++ stage2/03-mesh/01-run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -e
 
+#Custom RTL8192CU drivers
+RTLWIFI_DIR=lib/modules/4.4.38+/kernel/drivers/net/wireless/realtek/rtlwifi
+install -v -d											${ROOTFS_DIR}/${RTLWIFI_DIR}
+install -v -m 644 files/rtlwifi/rtlwifi.ko				${ROOTFS_DIR}/${RTLWIFI_DIR}/rtlwifi.ko
+install -v -m 644 files/rtlwifi/rtl_usb.ko				${ROOTFS_DIR}/${RTLWIFI_DIR}/rtl_usb.ko
+install -v -m 644 files/rtlwifi/rtl8192c-common.ko		${ROOTFS_DIR}/${RTLWIFI_DIR}/rtl8192c-common.ko
+install -v -m 644 files/rtlwifi/rtl8192cu.ko			${ROOTFS_DIR}/${RTLWIFI_DIR}/rtl8192cu.ko
+install -v -m 644 files/blacklist-rtl8192cu.conf		${ROOTFS_DIR}/etc/modprobe.d/blacklist-rtl8192cu.conf
+
 #Startup script
 install -v -d											${ROOTFS_DIR}/etc/mesh
 install -v -m 755 files/up.sh							${ROOTFS_DIR}/etc/mesh/
@@ -7,9 +16,9 @@ install -v -m 644 files/mesh							${ROOTFS_DIR}/etc/cron.d/
 
 #Web server config
 install -v -m 644 files/nginx.conf						${ROOTFS_DIR}/etc/nginx/nginx.conf
-install -v -m 644 files/mesh.local						${ROOTFS_DIR}/etc/nginx/sites-available/
 
 #Website files
 install -v -d											${ROOTFS_DIR}/var/www/mesh
+install -v -m 644 files/index.php						${ROOTFS_DIR}/var/www/mesh/index.php
 
 echo "Mesh config installed."
--- stage2/03-mesh/files/up.sh
+++ stage2/03-mesh/files/up.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+#Blink power LED
+echo gpio | sudo tee /sys/class/leds/led0/trigger
+echo 1 | sudo tee /sys/class/leds/led0/brightness
+sleep 1;
+echo 0 | sudo tee /sys/class/leds/led0/brightness
+sleep 1;
+echo 1 | sudo tee /sys/class/leds/led0/brightness
+sleep 1;
+
+
 #Connect to mesh network; script runs once at reboot
 #https://github.com/o11s/open80211s/wiki/HOWTO
-MESH_ID="meshnet"
+MESH_ID="MESHNET"
 
+ip link set down dev wlan0
 iw dev wlan0 interface add mesh0 type mp
 #ip link set dev mesh0 address $NEW_MAC_ADDR
 ip link set up dev mesh0
 iw dev mesh0 mesh join $MESH_ID
-#iw dev mesh0 station dump
+iw dev mesh0 station dump > /home/pi/mesh_stations
 
 #TODO: Get/set IP addresses? IPv6 NDP?
+
+
+#Set power LED on
+echo 0 | sudo tee /sys/class/leds/led0/brightness
