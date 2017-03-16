d0f90f2b3da283c009cb6552d1025843acb50bdf
Techwolf <dring@g33kworld.net>
2017-03-15 23:54:36 -0700

    Updated frontend interface.

--- stage2/03-mesh/files/index.php
+++ stage2/03-mesh/files/index.php
@@ -1,7 +1,28 @@
+<html>
+
+<head>
+	<title>Self-Sustaining Mesh Network</title>
+	<style>
+		body {
+			background-color: #222222;
+			color: #DDDDDD;
+		}
+		table {
+			border-collapse: collapse;
+			margin-top: 4px;
+		}
+		table, th, td {
+			border: 1px solid #DDDDDD;
+			padding: 4px;
+		}
+	</style>
+</head>
+
+<body>
 <?php
 	$datadir = "/var/lib/mesh";
 
-	echo "You are connected to: " . $_SERVER["SERVER_ADDR"] . "<p>\n";
+	print("<h2>You are connected to: <b>" . $_SERVER["SERVER_ADDR"] . "</b></h2><p>\n");
 
 	$nodes = array_diff(scandir($datadir), array(".", ".."));
 
@@ -9,8 +30,19 @@
 		$node = file_get_contents($datadir . "/" . $nodefile);
 		$data = json_decode($node, true);
 		$lastseen = time() - $data["time"];
-		print($nodefile . " (last seen " . $lastseen . "s ago):<br>\n");
-		print_r($data);
-		print("<p>\n");
+		print("Node <b>" . $nodefile . "</b> (last seen " . $lastseen . "s ago):<br>\n");
+
+		print("<table style=\"border: \"><tr>\n");
+		for($i = 0; $i < count($data["gpio"]); $i++) {
+			print("\t<td>" . "GPIO " . $i . "</td>\n");
+		}
+		print("</tr>\n<tr>\n");
+		foreach($data["gpio"] as $gpio) {
+			print("\t<td>" . $gpio . "</td>\n");
+		}
+		print("</tr></table><p>\n");
 	}
 ?>
+</body>
+
+</html>
